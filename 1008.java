a85ed42b0150954fc1e4e4be446a8a95b5f9fb6f
卓寿杰 <zhuoshoujie@126.com>
2016-07-10 11:40:17 +0800

    Create 1008.java

---
+++ 1008.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int nowFloor = 0;
+		int sumTime = 0;
+		for(int i = 0 ;i < n ;++i)
+		{
+			int num = in.nextInt();
+			int dis = num - nowFloor;
+			if(dis > 0)
+			{
+				sumTime += dis * 6 + 5;
+			}
+			else
+			{
+				sumTime += -dis * 4 + 5;
+			}
+			nowFloor = num;
+		}
+		System.out.println(sumTime);
+	}
+
+}
