6ba3bd92f25aa3236a1c09716a96e42e858cc512
卓寿杰 <zhuoshoujie@126.com>
2016-07-10 13:20:12 +0800

    Create 1005.java

---
+++ 1005.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String strarr[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		int NumSum = 0;
+		for (int i = 0; i < str.length(); ++i) {
+			NumSum += str.charAt(i) - '0';
+		}
+		String StrSum = String.valueOf(NumSum);
+		for (int i = 0; i < StrSum.length(); ++i) {
+			if (i == 0)
+				System.out.print(strarr[StrSum.charAt(i) - '0']);
+			else
+				System.out.print(" " + strarr[StrSum.charAt(i) - '0']);
+		}
+	}
+
+}
